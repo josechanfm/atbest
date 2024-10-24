57f4e2d2a528963b752ca1180951708e9a0739cb
josechanfm <josechan@ipm.edu.mo>
2024-10-24 10:15:14 +0800

    member card update

--- app/Http/Controllers/Member/MembershipController.php
+++ app/Http/Controllers/Member/MembershipController.php
@@ -26,9 +26,9 @@ class MembershipController extends Controller
     }
     public function switch(Member $member){
         Member::where('user_id',$member->user->id)->update(['default'=>false]);
-        $member->organization;
         $member->default=true;
         $member->save();
+        $member->organization;
         session(['member'=>$member]);
         return to_route('member.dashboard');
     }
